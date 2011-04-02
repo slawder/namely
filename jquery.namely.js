a57f523c8501bb99bf7b35849462e5836d81fe6e
Stuart Lawder <stuartlawder@gmail.com>
2011-04-02 11:05:27 -0400

    v 1.0.0

---
+++ jquery.namely.js
@@ -0,0 +1,43 @@
+/*
+jQuery Namely Plugin
+-------------------------
+Standardize name inputs
+
+Copyright (c) 2011 Stuart Lawder, Lawderberry Ltd.
+
+Version: 1.0.0 (4/2/2011)
+Requires: jQuery v1.3+
+
+http://www.lawderberry.com/namely
+
+*/
+
+(function($){
+    $.fn.extend({ 
+        namely: function() {
+            //Iterate over the current set of matched elements
+            return this.each(function() {
+                //code to be inserted here
+                $(this).blur(function(){
+                  var txt = $(this).val();
+                  //split txt into words
+                  var arr = txt.split(" ");
+                  var x = 0;
+                  for (x in arr){
+                    var str = arr[x];
+                    var firstLetter = str.substr(0, 1);
+                    var rest = str.substr(1);
+                    //if one letter is capp in rest of string leave it
+                    var y = rest.split(/[A-Z]/g);
+                    if(y.length > 2) {
+                      rest = rest.toLowerCase();
+                    } 
+                    arr[x] = firstLetter.toUpperCase() + rest;
+                  }
+                  txt = arr.join(" ");
+                  $(this).val(txt);
+                });
+            });
+        }
+    });
+})(jQuery);
